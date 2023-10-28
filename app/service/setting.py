e9818498eb64aecdb1a7ac59797ae9050810c119
Faris Fraeed Khaymi <farisfareed.k@gmail.com>
2023-10-28 17:15:32 +0300

    feat: binance api setting

--- app/service/setting.py
+++ app/service/setting.py
@@ -1,10 +1,14 @@
-from pydantic import Field
+from pydantic import Field, HttpUrl
 from pydantic_settings import BaseSettings
 
 
 class Setting(BaseSettings):
     TELEGRAM_TOKEN: str
     TELEGRAM_OWNER_ID: int
+
+    BINANCE_BASE_URL: HttpUrl
+    BINANCE_API_KEY: str
+    BINANCE_API_SECRET: str
 
     TARGET: int = Field(3, ge=1, le=6)
 
