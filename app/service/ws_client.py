3af3aa7196ba5ffa8dafb9ff8a3ed8882a53a96d
Faris Fraeed Khaymi <farisfareed.k@gmail.com>
2023-11-12 19:35:47 +0300

    feat: better websockets

--- app/service/ws_client.py
+++ app/service/ws_client.py
@@ -16,6 +16,7 @@ from .setting import setting as Setting
 
 log = get_logger(__name__)
 
+ws_client: UMFuturesWebsocketClient = None
 listen_key = ""
 
 
@@ -109,28 +110,25 @@ def message_handler(_: BinanceSocketManager, message: dict) -> None:
         log.error(f"Found error: {error}")
 
 
-ws_client = UMFuturesWebsocketClient(
-    stream_url=Setting.BINANCE_WS_BASE_URL, on_message=message_handler
-)
-
-listen_key = api_client.new_listen_key()["listenKey"]
-ws_client.user_data(
-    listen_key=listen_key,
-    id=int(datetime.now().timestamp()),
-)
-
-
 def renew_session() -> None:
     global ws_client
+    global listen_key
     while True:
         try:
+            if ws_client:
+                ws_client.stop()
             ws_client = UMFuturesWebsocketClient(
                 stream_url=Setting.BINANCE_WS_BASE_URL, on_message=message_handler
             )
-            log.debug("Session renewed")
+            listen_key = api_client.new_listen_key()["listenKey"]
+            ws_client.user_data(
+                listen_key=listen_key,
+                id=int(datetime.now().timestamp()),
+            )
+            log.debug(f"Session renewed for {listen_key}")
+            sleep(82800)
         except Exception as error:
             log.error(f"Found error: {error}")
-        sleep(82800)
 
 
 def renew_key() -> None:
@@ -146,9 +144,5 @@ def renew_key() -> None:
                 f"\nError code: {error.error_code}"
                 f"\nError message: {error.error_message}"
             )
-            listen_key = api_client.new_listen_key()["listenKey"]
-            ws_client.user_data(
-                listen_key=listen_key,
-                id=int(datetime.now().timestamp()),
-            )
-            sleep(3300)
+        except Exception as error:
+            log.error(f"Found error: {error}")
